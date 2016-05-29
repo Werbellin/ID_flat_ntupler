b89427960accc9e098f5791a08db95b6e921995b
Philipp Pigard <pigard@llr.in2p3.fr>
2016-05-29 10:04:27 +0200

    CHange name

--- Ntuplizer/python/run_cfg.py
+++ Ntuplizer/python/run_cfg.py
@@ -1,3 +1,12 @@
+# Create a variable named "name" with value equal to default, only if such a variable is not yet defined.
+
+def declareDefault(name, default, glb):
+    if name not in glb:
+        glb[name]=default
+
+declareDefault("OUTPUTPATH", '/data/DATA/temp_pigard/eID/new_ntup_test/ggH_MINIAODSIM_Chunk0/', globals())
+#declareDefault("OUTPUTPATH", '/home/llr/cms/pigard/EGM/NT_vs_T_MVA/new_ntup/CMSSW_7_6_4/src/Analyzer/Ntuplizer/python/', globals())
+
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
@@ -26,7 +35,7 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
 # turn on VID producer, indicate data format  to be
 # DataFormat.AOD or DataFormat.MiniAOD, as appropriate 
-fileFormat = 'AOD'
+fileFormat = 'MiniAOD'
 
 if fileFormat == 'AOD' :
     dataFormat = DataFormat.AOD
@@ -34,6 +43,7 @@ else :
     dataFormat = DataFormat.MiniAOD
 
 switchOnVIDElectronIdProducer(process, dataFormat)
+switchOnVIDPhotonIdProducer(process, dataFormat)
 
 # define which IDs we want to produce
 my_id_modules = [
@@ -48,10 +58,30 @@ my_id_modules = [
 for idmod in my_id_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
 
+switchOnVIDPhotonIdProducer(process, dataFormat)
+# define which IDs we want to produce
+my_id_modules = [
+                 'RecoEgamma.PhotonIdentification.Identification.mvaPhotonID_Spring15_25ns_nonTrig_V2_cff',
+                 'RecoEgamma.PhotonIdentification.Identification.mvaTLEID_Fall15_V1_cff',
+               ]
+
+#add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
 
 
-process.ntuple = cms.EDAnalyzer('Ntuplizer',
+# Setup ISO calculation
+from RecoEgamma.EgammaIsolationAlgos.egmPhotonIsolationMiniAOD_cff import egmPhotonIsolationMiniAOD
+
+process.egmPhotonIsolationMiniAOD = egmPhotonIsolationMiniAOD.clone()
+
+
+#process.TFileService = cms.Service("TFileService", fileName = cms.string('/data/DATA/temp_pigard/eID/') )
+
+tle_ntuple = cms.EDAnalyzer('Ntuplizer',
                                    inputFileFormat = cms.untracked.string(fileFormat),
+                                   outputPath = cms.string(OUTPUTPATH), #process.TFileService.fileName,
+                                   outputFile = cms.string('TLE'),
                                    beamSpot = cms.InputTag('offlineBeamSpot'),
                                    # input collection names AOD
                                    electronsAOD = cms.InputTag('gedGsfElectrons'),
@@ -65,14 +95,21 @@ process.ntuple = cms.EDAnalyzer('Ntuplizer',
                                    esReducedRecHitCollectionAOD = cms.InputTag("reducedEcalRecHitsES"),
  
                                    electronsMiniAOD = cms.InputTag('slimmedElectrons'),
+                                   photonsMiniAOD = cms.InputTag('slimmedPhotons'),
                                    verticesMiniAOD = cms.InputTag('offlineSlimmedPrimaryVertices'),
                                    conversionsMiniAOD = cms.InputTag('reducedEgamma:reducedConversions'),
                                    genParticlesMiniAOD = cms.InputTag('prunedGenParticles'), 
                                    genEventInfoProductMiniAOD = cms.InputTag('generator'),
                                    PFMETMiniAOD = cms.InputTag('slimmedMETs'),                                
+                                   ebReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedEBRecHits"),
+                                   eeReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedEERecHits"),
+                                   esReducedRecHitCollectionMiniAOD = cms.InputTag("reducedEgamma:reducedESRecHits"),
 
                                    HLTTag          = cms.InputTag('TriggerResults','','HLT'),
                                    isMC = cms.bool(True),
+                                   do_signal = cms.bool(True),
+                                   do_TLE = cms.bool(True),
+                                   do_min_dR_cut = cms.bool(True),
                                    MVAId  = cms.VInputTag(),
                                    ID1_use_userFloat = cms.bool(False),
                                    electronID1 = cms.string("ElectronMVAEstimatorRun2Spring15NonTrig25nsV1"),
@@ -81,18 +118,43 @@ process.ntuple = cms.EDAnalyzer('Ntuplizer',
                                    electronID1_pass = cms.InputTag("egmGsfElectronIDs:mvaEleID-Spring15-25ns-nonTrig-V1-wp90"),
                                    electronID2_pass = cms.InputTag("egmGsfElectronIDs:mvaEleID-Spring15-25ns-nonTrig-V1-wp90"),
 
+                                   photonID1 = cms.string("PhotonMVAEstimatorRun2Spring15NonTrig25nsV2"),
+                                   photonID2 = cms.string("TLEMVAEstimatorRun2Fall15V1"),
 
 )
+
+process.tle_signal = tle_ntuple.clone()
+process.tle_signal.do_signal = cms.bool(True)
+
+process.tle_background = tle_ntuple.clone()
+process.tle_background.do_signal = cms.bool(False)
+
+tle_ntuple.outputFile = cms.string('REG')
+tle_ntuple.do_TLE = cms.bool(False)
+
+process.reg_signal = tle_ntuple.clone()
+process.reg_signal.do_signal = cms.bool(True)
+
+process.reg_background = tle_ntuple.clone()
+process.reg_background.do_signal = cms.bool(False)
+
+
+
 fileNameForSample = 'ntuple'
 
 
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
 #    fileNames = cms.untracked.vstring('file:/data_CMS/cms/davignon/Trigger_WithThomas/CMSSW_7_6_0_pre7/src/L1Trigger/L1TNtuples/00BEC5EF-1472-E511-806C-02163E0141EA.root')
 fileNames = cms.untracked.vstring(
-'/store/mc/RunIIFall15DR76/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/00000/08DC4220-16A7-E511-AF59-1CC1DE19286E.root')
+#'/store/mc/RunIIFall15DR76/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/00000/08DC4220-16A7-E511-AF59-1CC1DE19286E.root')
+'/store/mc/RunIIFall15MiniAODv2/GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/50000/0AFC3AB4-96B9-E511-AE01-5065F3817221.root',
+#'/store/mc/RunIIFall15MiniAODv2/GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV6_pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/50000/06A78A71-97B9-E511-A22A-24BE05C3CBD1.root',
+
+
+)
 
 #'/store/mc/RunIIFall15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/70000/002ABFCA-A0B9-E511-B9BA-0CC47A57CD6A.root')
 #/store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/60000/02CDE51A-726D-E511-B2C4-0025905C96EA.root')
@@ -101,13 +163,10 @@ fileNames = cms.untracked.vstring(
 
 #fileNames = cms.untracked.vstring('/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/Asympt25ns_MCRUN2_74_V9-v1/50000/0AD4F3AF-CE36-E511-AC8A-008CFA1974CC.root')
 
-#fileNames = cms.untracked.vstring('file:test.root')
 
 )
  
-#process.GlobalTag.globaltag = ' MCRUN2_74_V8::All'
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-
 process.GlobalTag = GlobalTag(process.GlobalTag, '76X_mcRun2_asymptotic_v12', '') # MCRUN2_74_V8
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string(fileNameForSample + '.root') )
@@ -136,4 +195,4 @@ process.MessageLogger = cms.Service(
     )
 )
 
-process.p = cms.Path(process.electronMVAValueMapProducer *  process.ntuple)#*process.dump)
+process.p = cms.Path(process.egmPhotonIDSequence * process.electronMVAValueMapProducer * process.egmPhotonIsolationMiniAOD * process.reg_signal * process.reg_background* process.tle_signal * process.tle_background)#*process.dump)
